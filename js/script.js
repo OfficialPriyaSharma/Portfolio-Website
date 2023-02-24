837cbe47bf8c48dcad11e3b94e1a6ee3c371f852
Priya Sharma <101420743+OfficialPriyaSharma@users.noreply.github.com>
2023-02-24 20:10:17 +0530

    portfolio website

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["Associate Software Developer","Software Engineer","Full Stack Software Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
