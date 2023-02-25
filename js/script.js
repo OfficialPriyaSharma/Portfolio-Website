20c2f08d076d0deedc5287280f00b927dc587e9c
Priya Sharma <101420743+OfficialPriyaSharma@users.noreply.github.com>
2023-02-25 15:28:41 +0530

    portfolio website

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ var typed = new Typed(".typing",{
 const nav = document.querySelector(".nav"),
         navList = nav.querySelectorAll("li"),
         totalNavList = navList.length;
+        allSection = document.querySelectorAll(".section"),
+        totalSection = allSection.length;
         for(let i=0; i<totalNavList; i++)
         {
             const a = navList[i].querySelector("a");
@@ -15,9 +17,22 @@ const nav = document.querySelector(".nav"),
                 {
                     for(let j=0; j<totalNavList; j++)
                     {
+                        if(navList[j].querySelector("a").classList.contains("active"))
+                        {
+                            console.log(navList[j].querySelector("a"))
+                        }
                         navList[j].querySelector("a").classList.remove("active");
                     }
                     this.classList.add("active")
-                
+                    showSection(this);
                 })
+        }
+        function showSection(element)
+        {
+            for(let i=0; i<totalSection; i++)
+            {
+                allSection[i].classList.remove("active");
+            }
+            const target = element.getAttribute("href").split('#')[1];
+            document.querySelector("#" + target).classList.add("active")
         }
