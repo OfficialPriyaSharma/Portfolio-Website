063b9f918404bd6a11ecb3385fca2ea14e822b30
Priya Sharma <101420743+OfficialPriyaSharma@users.noreply.github.com>
2023-02-25 16:00:27 +0530

    portfolio website

--- js/script.js
+++ js/script.js
@@ -15,11 +15,15 @@ const nav = document.querySelector(".nav"),
             const a = navList[i].querySelector("a");
             a.addEventListener("click", function()
                 {
+                    for(let i=0; i<totalSection; i++)
+            {
+                allSection[i].classList.remove("back-section");
+            }
                     for(let j=0; j<totalNavList; j++)
                     {
                         if(navList[j].querySelector("a").classList.contains("active"))
                         {
-                            console.log(navList[j].querySelector("a"))
+                            allSection[j].classList.add("back-section");
                         }
                         navList[j].querySelector("a").classList.remove("active");
                     }
@@ -36,3 +40,18 @@ const nav = document.querySelector(".nav"),
             const target = element.getAttribute("href").split('#')[1];
             document.querySelector("#" + target).classList.add("active")
         }
+        const navTogglerBtn = document.querySelector(".nav-toggler"),
+            aside = document.querySelector(".aside");
+            navTogglerBtn.addEventListener("click", () =>
+            {
+                asideSectionTogglerBtn();
+            })
+            function asideSectionTogglerBtn()
+            {
+                aside.classList.toggle("open");
+                navTogglerBtn.classList.toggle("open");
+                for(let i=0; i<totalSection; i++)
+                {
+                    allSection[i].classList.toggle("open");
+                }
+            }
